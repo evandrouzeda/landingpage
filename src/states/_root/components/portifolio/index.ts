1e5dc6f0e9d9f6a26f5c84c196ecf03a9267e3cc
Evandro Uzeda <evandrouzeda@gmail.com>
2024-06-04 21:06:05 +0000

    mostrando portifolio com animação no scroll

--- src/states/_root/components/portifolio/index.ts
+++ src/states/_root/components/portifolio/index.ts
@@ -4,25 +4,55 @@ import App from "../../../../app";
 import style from "./style.module.css"
 
 export default class Portifolio extends LayoutContainerSection {
-    constructor(app: App){
+    actived = false
+    lastscroll = 0
+    direction = false
+
+    constructor(app: App) {
         super()
         this.class(style.portifolio).children(
-            Z("div").class(style.primeiro, style.verde).children(
+            Z("div").class(style.primeiro, style.verde, style.left).children(
                 Z("p").text("Landing Page"),
                 Z("div")
             ),
-            Z("div").class(style.segundo, style.amarelo).children(
+            Z("div").class(style.segundo, style.amarelo, style.top).children(
                 Z("p").text("Aplicativo para automação residencial"),
                 Z("div")
             ),
-            Z("div").class(style.terceiro, style.azul).children(
+            Z("div").class(style.terceiro, style.azul, style.bottom).children(
                 Z("p").text("Skill Alexa para integrar via Lambda com a Plataforma de automação residencial"),
                 Z("div")
             ),
-            Z("div").class(style.quarto, style.vermelho).children(
+            Z("div").class(style.quarto, style.vermelho, style.right).children(
                 Z("p").text("Aplicativo para festival de Restaurantes"),
                 Z("div")
             ),
-        )
+        ).object(async (o) => {
+            window.addEventListener("scroll", () => {
+                this.element.getBoundingClientRect()
+                const goingUP = this.lastscroll > window.scrollY
+                const start = window.innerHeight + window.scrollY >= this.element.offsetTop - 50
+                const finish = window.innerHeight + window.scrollY <= this.element.offsetTop + 50
+                if (((start && finish) && !this.actived) || this.lastscroll === 0 && !goingUP) {
+                    this.actived = true;
+                    Array(this.element.children.length).fill("").forEach(async (v, i) => {
+                        await this.sleep(100 * i)
+                        this.element.children[i].classList.toggle(style.show)
+                    });
+                    //this.element.classList.toggle(style.show)
+                    console.log("Entrou", this.actived)
+                } else if (goingUP !== this.direction) this.actived = false
+                this.lastscroll = window.scrollY
+                this.direction = goingUP
+            })
+        })
+    }
+
+    sleep(time: number) {
+        return new Promise(res => {
+            setTimeout(() => {
+                res(true)
+            }, time);
+        })
     }
 }
