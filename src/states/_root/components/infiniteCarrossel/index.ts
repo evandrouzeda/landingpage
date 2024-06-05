ec0ee5cb9ca243c3cf9a93d6fc17c372de42a4e3
Evandro Uzeda <evandrouzeda@gmail.com>
2024-06-05 17:57:34 +0000

    resolvido problema com layout

--- src/states/_root/components/infiniteCarrossel/index.ts
+++ src/states/_root/components/infiniteCarrossel/index.ts
@@ -7,7 +7,7 @@ export default class InfiniteCarrossel extends LayoutContainerFullWidth<"section
         super("section")
         this.class(style.section).children(
             Z("h2").class(this.layout.free).text(title),
-            Z("div").class(style.brands, this.layout.free, this.layout.fullWidth).children(
+            Z("div").class(style.brands, this.layout.free, this.layout.fullWidthOnly).children(
                 ...Array(6).fill("").map(() => Z("span").class(images.length > 5 ? style.slow : style.fast).children(
                     ...images.map(i => Z("img").set("src", `/_img/${i}`))
                 ))
--- src/states/_root/components/portifolio/index.ts
+++ src/states/_root/components/portifolio/index.ts
@@ -11,50 +11,52 @@ export default class Portifolio extends LayoutContainerSection {
 
     constructor(app: App) {
         super()
-        this.class(style.portifolio).children(
-            Z("h2").text("Implementou").class(this.layout.free),
-            Z("div").class(style.primeiro, style.verde, style.left).children(
-                Z("b").text("Landing Page"),
-                Z("div")
-            ),
-            Z("div").class(style.segundo, style.amarelo, style.top).object(o => {
-                o.children(
-                    Z("b").text("Aplicativo para automação residencial"),
-                    Z("div").class(style.automacao).children(
-                        Z("h2").text("Luz da Sala"),
-                        new ToggleSwitch(app).checked().onChange(() => {
-                            o.element.classList.toggle(style.cinza)
-                        })
+        this.class(style.portifolio, this.layout.fullWidth).children(
+            Z("h2").text("Já implementou").class(this.layout.free),
+            Z("div").class(style.container).children(
+                Z("div").class(style.primeiro, style.verde, style.left).children(
+                    Z("b").text("Landing Page"),
+                    Z("div")
+                ),
+                Z("div").class(style.segundo, style.amarelo, style.top).object(o => {
+                    o.children(
+                        Z("b").text("Aplicativo para automação residencial"),
+                        Z("div").class(style.automacao).children(
+                            Z("h2").text("Luz da Sala"),
+                            new ToggleSwitch(app).checked().onChange(() => {
+                                o.element.classList.toggle(style.cinza)
+                            })
+                        )
                     )
-                )
-            }),
-            Z("div").class(style.terceiro, style.azul, style.bottom).children(
-                Z("b").text("Skill Alexa para integrar via Lambda com a Plataforma de automação residencial"),
-                Z("div")
-            ),
-            Z("div").class(style.quarto, style.vermelho, style.right).children(
-                Z("b").text("Aplicativo para festival de Restaurantes"),
-                Z("div")
-            ),
-        ).object(async (o) => {
-            window.addEventListener("scroll", () => {
-                this.element.getBoundingClientRect()
-                const goingUP = this.lastscroll > window.scrollY
-                const start = window.innerHeight + window.scrollY >= this.element.offsetTop - 50
-                const finish = window.innerHeight + window.scrollY <= this.element.offsetTop + 50
-                if (((start && finish) && !this.actived) || this.lastscroll === 0 && !goingUP) {
-                    this.actived = true;
-                    Array(this.element.children.length).fill("").forEach(async (v, i) => {
-                        await this.sleep(100 * i)
-                        this.element.children[i].classList.toggle(style.show)
-                    });
-                    //this.element.classList.toggle(style.show)
-                    console.log("Entrou", this.actived)
-                } else if (goingUP !== this.direction) this.actived = false
-                this.lastscroll = window.scrollY
-                this.direction = goingUP
+                }),
+                Z("div").class(style.terceiro, style.azul, style.bottom).children(
+                    Z("b").text("Skill Alexa para integrar via Lambda com a Plataforma de automação residencial"),
+                    Z("div")
+                ),
+                Z("div").class(style.quarto, style.vermelho, style.right).children(
+                    Z("b").text("Aplicativo para festival de Restaurantes"),
+                    Z("div")
+                ),
+            ).object(async (o) => {
+                window.addEventListener("scroll", () => {
+                    o.element.getBoundingClientRect()
+                    const goingUP = this.lastscroll > window.scrollY
+                    const start = window.innerHeight + window.scrollY >= o.element.offsetTop - 50
+                    const finish = window.innerHeight + window.scrollY <= o.element.offsetTop + 50
+                    if (((start && finish) && !this.actived) || this.lastscroll === 0 && !goingUP) {
+                        this.actived = true;
+                        Array(o.element.children.length).fill("").forEach(async (v, i) => {
+                            await this.sleep(100 * i)
+                            o.element.children[i].classList.toggle(style.show)
+                        });
+                        //o.element.classList.toggle(style.show)
+                        console.log("Entrou", this.actived)
+                    } else if (goingUP !== this.direction) this.actived = false
+                    this.lastscroll = window.scrollY
+                    this.direction = goingUP
+                })
             })
-        })
+        )
     }
 
     sleep(time: number) {
